d38897d2efc6d096f810f516446fabb71af30108
zr-zhou0o0 <2033616887@qq.com>
2025-02-26 01:05:02 +0800

    change weights path

--- efficient_sam/build_efficient_sam.py
+++ efficient_sam/build_efficient_sam.py
@@ -6,18 +6,17 @@
 
 from .efficient_sam import build_efficient_sam
 
-def build_efficient_sam_vitt():
-    return build_efficient_sam(
-        encoder_patch_embed_dim=192,
-        encoder_num_heads=3,
-        checkpoint="weights/efficient_sam_vitt.pt",
-    ).eval()
-
-
 
 def build_efficient_sam_vits():
     return build_efficient_sam(
         encoder_patch_embed_dim=384,
         encoder_num_heads=6,
-        checkpoint="weights/efficient_sam_vits.pt",
+        checkpoint="weights/efficient_sam_vits.pt" ,
+    ).eval()
+
+def build_efficient_sam_vitt():
+    return build_efficient_sam(
+        encoder_patch_embed_dim=192,
+        encoder_num_heads=3,
+        checkpoint="weights/efficient_sam_vitt.pt",
     ).eval()
